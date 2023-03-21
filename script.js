71f3cde1673c8b35fbb46f95fab29232caf27ec9
Anton Napylov <antonio8845@gmail.com>
2023-03-21 20:51:32 +0300

    create get request

---
+++ script.js
@@ -0,0 +1,41 @@
+const url = 'https://intership-liga.ru/tasks/';
+
+// const resPromisePost = fetch(url, {
+//   method: 'POST',
+//   headers: {
+//     'content-type': 'application/json',
+//   },
+//   body: JSON.stringify({
+//     id: 600,
+//     name: 'CLN',
+//     info: 'Lorem ipsum dolor sit amet.',
+//     isImportant: false,
+//     isComplited: true,
+//   }),
+// });
+// resPromisePost
+//   .then((res) => res.json())
+//   .then((res) => console.log('Запись создана \n', res))
+//   .catch((err) => console.log(err));
+
+// class BaseAgent {
+//   constructor(option) {
+//     this.option = option;
+//   }
+//   async resPromisePost() {
+//     let res = await fetch(url, {
+//       method: 'GET',
+//     });
+//     let response = await res.json();
+//     console.log(response, this.option);
+//   }
+// }
+
+// class Agent extends BaseAgent {
+//   constructor(option) {
+//     super(option);
+//   }
+// }
+
+// let a = new Agent('hi');
+// a.resPromisePost();
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,70 @@
-console.log('Домашка');
+type body = {
+  id: number;
+  name: string;
+  info: string;
+  isImportant: boolean;
+  isComplited: boolean;
+};
+class BaseAgent {
+  baseUrl = 'https://intership-liga.ru/tasks/';
+  method: string;
+  body: body;
+
+  constructor(method: string, body: body) {
+    this.method = method;
+    this.body = body;
+  }
+
+  getMethod(): void {
+    console.log(this.method, this.body);
+  }
+}
+
+class ReqAgent extends BaseAgent {
+  constructor(method: string, body: body) {
+    super(method, body);
+  }
+
+  async resPromiseGet(): Promise<void> {
+    try {
+      const res = await fetch(this.baseUrl, {
+        method: this.method,
+      });
+      const time = new Date();
+      const response = await res.json();
+      console.log(response, time);
+    } catch {
+      throw new Error('Error');
+    }
+  }
+  async resPromisePost(): Promise<void> {
+    try {
+      const res = await fetch(this.baseUrl, {
+        method: 'POST',
+        headers: {
+          'content-type': 'application/json',
+        },
+        body: JSON.stringify(this.body),
+      });
+      console.log(this.body);
+      const time = new Date();
+      const response = await res.json();
+      console.log(response, time);
+    } catch (e) {
+      throw new Error(e);
+    }
+  }
+}
+
+const reqBody: body = {
+  id: 600,
+  name: 'Client',
+  info: 'Lorem ipsum dolor sit amet consectetur.',
+  isImportant: false,
+  isComplited: true,
+};
+
+const a = new ReqAgent('GET', reqBody);
+a.getMethod();
+// a.resPromisePost();
+a.resPromiseGet();
